16cf968ea22c5e5bd6c5f4f415277908d2f49014
らんるん <ss0302winning@gmail.com>
2022-02-07 04:39:46 +0900

    配置完成

---
+++ dist/assets/main.4ce09384.js
@@ -0,0 +1,97 @@
+import{S as d,i as r,s as m,e as o,a as n,b as p,n as a,d as h}from"./vendor.3bae58bb.js";const u=function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const l of document.querySelectorAll('link[rel="modulepreload"]'))i(l);new MutationObserver(l=>{for(const s of l)if(s.type==="childList")for(const c of s.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&i(c)}).observe(document,{childList:!0,subtree:!0});function t(l){const s={};return l.integrity&&(s.integrity=l.integrity),l.referrerpolicy&&(s.referrerPolicy=l.referrerpolicy),l.crossorigin==="use-credentials"?s.credentials="include":l.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function i(l){if(l.ep)return;l.ep=!0;const s=t(l);fetch(l.href,s)}};u();function g(v){let e;return{c(){e=o("main"),e.innerHTML=`<div class="header svelte-5mclll"><div class="wrapper svelte-5mclll"><h1>Thank you for watching!</h1> 
+      <p class="svelte-5mclll">\u4ECA\u306E\u81EA\u5206\u306E\u7D4C\u9A13\u3001\u4F5C\u54C1\u7B49\u3092\u307E\u3068\u3081\u307E\u3057\u305F\u3002</p></div></div> 
+
+  
+  <div class="contents svelte-5mclll"><div class="work black svelte-5mclll" data-works-id="training"><input type="checkbox" class="svelte-5mclll"/> 
+      <div class="headline svelte-5mclll"><h2>Training</h2> 
+        <h3>\u7814\u4FEE\u3067\u30BF\u30B9\u30AF\u30A2\u30D7\u30EA\u3092\u4F5C\u6210</h3> 
+        <p class="svelte-5mclll">\u30C1\u30FC\u30E0\u958B\u767A\u306E\u57FA\u672C\u3092\u5B66\u3076</p></div> 
+      <div style="background-image: url(&#39;../static/image/training.png&#39;)" alt="training" class="image svelte-5mclll"></div> 
+      <div class="message svelte-5mclll">\u30E1\u30C3\u30BB\u30FC\u30B8\u3067\u3059</div></div>  
+    <div class="work white svelte-5mclll" data-works-id="login"><input type="checkbox" class="svelte-5mclll"/> 
+      <div class="headline svelte-5mclll"><h2>LoginManageSystem</h2> 
+        <h3>\u793E\u54E1\u306E\u30ED\u30B0\u30A4\u30F3\u3001\u30ED\u30B0\u30AA\u30D5\u306E\u30ED\u30B0\u3092\u53D6\u5F97\u3057\u7BA1\u7406\u3059\u308B\u30A2\u30D7\u30EA\u306E\u30D0\u30C3\u30C1\u90E8\u5206\u3092\u4F5C\u6210</h3> 
+        <p class="svelte-5mclll">DB\u306E\u57FA\u672C\u3092\u5B66\u3073\u3001\u30D0\u30C3\u30C1\u30D5\u30A1\u30A4\u30EB\u3092\u77E5\u308B</p></div> 
+
+      <div style="background-image: url(&#39;../static/image/login.png&#39;)" alt="login" class="image svelte-5mclll"></div> 
+
+      <div class="message svelte-5mclll">\u30E1\u30C3\u30BB\u30FC\u30B8\u3067\u3059</div></div> 
+    <div class="work white svelte-5mclll" data-works-id="system"><input type="checkbox" class="svelte-5mclll"/> 
+      <div class="headline svelte-5mclll"><h2>SalesManegementSystem</h2> 
+        <h3>\u58F2\u308A\u4E0A\u3052\u7BA1\u7406\u30B7\u30B9\u30C6\u30E0\u306E\u5927\u90E8\u5206\u3092\u4F5C\u6210</h3> 
+        <p class="svelte-5mclll">Web\u30A2\u30D7\u30EA\u306E\u57FA\u672C\u3092\u5B66\u3076</p></div> 
+
+      <div style="background-image: url(&#39;../static/image/system.png&#39;)" alt="login" class="image svelte-5mclll"></div> 
+
+      <div class="message svelte-5mclll">\u30E1\u30C3\u30BB\u30FC\u30B8\u3067\u3059</div></div>  
+    <div class="work black svelte-5mclll" data-works-id="vuelidate"><input type="checkbox" class="svelte-5mclll"/> 
+      <div class="headline svelte-5mclll"><h2>WorkEfficiencyApp</h2> 
+        <h3>Excel\u306E\u30C7\u30FC\u30BF\u3092DB\u306B\u79FB\u884C\u3001\u7BA1\u7406\u3059\u308B\u30C4\u30FC\u30EB\u306E\u4F5C\u6210</h3> 
+        <p class="svelte-5mclll">\u4E0A\u6D41\u5DE5\u7A0B\u304B\u3089\u306E\u4E00\u9023\u306E\u6D41\u308C\u3092\u5B66\u3076</p></div> 
+
+      <div style="background-image: url(&#39;../static/image/vuelidate.png&#39;)" alt="login" class="image svelte-5mclll"></div> 
+
+      <div class="message svelte-5mclll">\u30E1\u30C3\u30BB\u30FC\u30B8\u3067\u3059</div></div></div> 
+
+  <div class="header svelte-5mclll"><div class="wrapper svelte-5mclll"><p class="svelte-5mclll">\u4EE5\u4E0A\u304C\u524D\u8077\u3067\u306E1\u5E74\u9593\u306E\u7D4C\u9A13\u3067\u3059\u3002</p> 
+      <p class="svelte-5mclll">\u6B21\u306F\u3001\u3053\u308C\u304B\u3089\u306E\u5C31\u8077\u306B\u5411\u3051\u3066\u4F5C\u6210\u3057\u305F\u4F5C\u54C1\u3092\u3054\u89A7\u304F\u3060\u3055\u3044\u3002</p></div></div> 
+
+  <div class="contents svelte-5mclll"><div class="work black svelte-5mclll" data-works-id="website"><a href="https://github.com/onion2424/portfolio-web" target="_blank" rel="noopener noreferrer" class="svelte-5mclll"></a> 
+      <input type="checkbox" class="svelte-5mclll"/> 
+      <div class="headline svelte-5mclll"><h2>WebSite</h2> 
+        <h3>\u30DD\u30FC\u30C8\u30D5\u30A9\u30EA\u30AA\u3068\u3057\u3066\u4F5C\u6210</h3> 
+        <p class="svelte-5mclll">Web\u30B5\u30A4\u30C8\u3092\u81EA\u529B\u3067\u7ACB\u3061\u4E0A\u3052\u308B\u96E3\u3057\u3055\u3092\u77E5\u308B</p></div> 
+      <div style="background-image: url(&#39;../static/image/this_site.png&#39;)" alt="this_site" class="image svelte-5mclll"></div> 
+      <div class="message svelte-5mclll">\u30E1\u30C3\u30BB\u30FC\u30B8\u3067\u3059</div></div> 
+    <div class="work white svelte-5mclll" data-works-id="later-do"><a href="https://github.com/onion2424/later-do" target="_blank" rel="noopener noreferrer" class="svelte-5mclll"></a> 
+      <input type="checkbox" class="svelte-5mclll"/> 
+      <div class="headline svelte-5mclll"><h2>LineBot</h2> 
+        <h3>\u30DD\u30FC\u30C8\u30D5\u30A9\u30EA\u30AA\u3068\u3057\u3066\u4F5C\u6210</h3> 
+        <p class="svelte-5mclll">\u5B9F\u50CD\u3059\u308B\u30B7\u30B9\u30C6\u30E0\u306E\u7ACB\u3061\u4E0A\u3052\u306E\u96E3\u3057\u3055\u3092\u77E5\u308B</p></div> 
+      <div style="background-image: url(&#39;../static/image/later_do.png&#39;)" alt="later_do" class="image svelte-5mclll"></div> 
+      <div class="message svelte-5mclll">\u30E1\u30C3\u30BB\u30FC\u30B8\u3067\u3059</div></div></div> 
+
+  <div class="header svelte-5mclll"><div class="wrapper svelte-5mclll"><p class="svelte-5mclll">\u4E0A\u306E\u30E1\u30A4\u30F3\u306E\u4F5C\u54C1\u306F2\u3064\u3068\u3082github\u306B\u30B3\u30FC\u30C9\u3084\u3001\u8AAC\u660E\u3092\u8F09\u305B\u3066\u3044\u307E\u3059\u3002\u3088\u308D\u3057\u3051\u308C\u3070\u898B\u3066\u3044\u3063\u3066\u304F\u3060\u3055\u3044\u3002</p> 
+      <p class="svelte-5mclll">\u3053\u308C\u4EE5\u964D\u306F\u81EA\u5DF1\u7D39\u4ECB\u306E\u3088\u3046\u306A\u3082\u306E\u306A\u306E\u3067\u3001\u6C17\u697D\u306B\u3069\u3046\u305E\u3002</p></div></div> 
+  
+  <div class="skills svelte-5mclll"><div class="skill svelte-5mclll"><div class="svelte-5mclll"><h2>HTML/CSS</h2> 
+        <p class="svelte-5mclll">CSS\u306F\u82E6\u624B</p></div></div> 
+    <div class="skill svelte-5mclll"><div class="svelte-5mclll"><h2>JavaScript</h2></div></div> 
+    <div class="skill svelte-5mclll"><div class="svelte-5mclll"><h2>Backend</h2></div></div> 
+    <div class="skill svelte-5mclll"><div class="svelte-5mclll"><h2>DataBase</h2></div></div> 
+    <div class="skill svelte-5mclll"><div class="svelte-5mclll"><h2>Infrastructure</h2></div></div> 
+    <div class="skill svelte-5mclll"><div class="svelte-5mclll"><h2>Information Technology</h2></div></div></div> 
+
+  <div class="header svelte-5mclll"><div class="wrapper svelte-5mclll"><p class="svelte-5mclll">\u30D7\u30ED\u30B0\u30E9\u30DF\u30F3\u30B0\u81EA\u4F53\u3084\u30A2\u30EB\u30B4\u30EA\u30BA\u30E0\u3092\u8003\u3048\u308B\u3053\u3068\u304C\u597D\u304D\u306A\u306E\u3067\u8A00\u8A9E\u306B\u3053\u3060\u308F\u3089\u305A\u904A\u3093\u3067\u3044\u307E\u3059\u3002</p></div></div> 
+  
+  <div class="contents svelte-5mclll"><div class="work black svelte-5mclll" data-works-id="MakeCode"><input type="checkbox" class="svelte-5mclll"/> 
+      <div class="headline svelte-5mclll"><h2>MakeCode</h2> 
+        <h3>MineCraft\u30673\u3064\u306E\u30D6\u30ED\u30C3\u30AF\u3092\u5F53\u3066\u308B\u30B2\u30FC\u30E0</h3> 
+        <p class="svelte-5mclll">\u751F\u5F92\u306B\u300C\u30D7\u30ED\u30B0\u30E9\u30DF\u30F3\u30B0\u3063\u3066\u4F55\u304C\u51FA\u6765\u308B\u306E\uFF1F\u300D\u3068\u805E\u304B\u308C\u305F\u306E\u3067\u4F5C\u6210</p></div> 
+      <div class="video svelte-5mclll"><div class="video_container youtube svelte-5mclll"><div class="iframe_wrapper svelte-5mclll"><iframe src="https://www.youtube.com/embed/hLxjYZYNBZ8?rel=0&amp;mute=1&amp;playsinline=1" title="Youtube video player" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" class="svelte-5mclll"></iframe></div></div></div> 
+      <div class="message svelte-5mclll">\u30E1\u30C3\u30BB\u30FC\u30B8\u3067\u3059</div></div> 
+    
+    <div class="work black svelte-5mclll" data-works-id="MakeCode"><input type="checkbox" class="svelte-5mclll"/> 
+      <div class="headline svelte-5mclll"><h2>HumanResourceMachine</h2> 
+        <h3>\u96E3\u3057\u3044\u3068\u8A55\u5224\u3060\u3063\u305F\u306E\u3067\u3084\u3063\u3066\u307F\u305F</h3> 
+        <p class="svelte-5mclll">\u5168\u90E8\u89E3\u3044\u305F\u8A18\u5FF5\u306B\u6C17\u306B\u5165\u3063\u3066\u3044\u308B\u6700\u5F8C\u306E\u554F\u984C\u306E\u56DE\u7B54\u3092\u8F09\u305B\u3066\u304A\u304F</p></div> 
+      <div class="video svelte-5mclll"><div class="video_container youtube svelte-5mclll"><div class="iframe_wrapper svelte-5mclll"><iframe src="https://www.youtube.com/embed/_3utm73T4Ls?rel=0&amp;mute=1&amp;playsinline=1" title="Youtube video player" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" class="svelte-5mclll"></iframe></div></div></div> 
+      <div class="message svelte-5mclll">\u30E1\u30C3\u30BB\u30FC\u30B8\u3067\u3059</div></div></div> 
+
+  <div class="header svelte-5mclll"><div class="wrapper svelte-5mclll"><p class="svelte-5mclll">\u6700\u5F8C\u306B\u3001\u3053\u308C\u307E\u3067\u306E\u65E5\u5E38\u3067\u751F\u307E\u308C\u305F\u4F5C\u54C1\u3092\u7D39\u4ECB\u3057\u307E\u3059\u3002</p></div></div> 
+
+  <div class="contents svelte-5mclll"><div class="work black svelte-5mclll" data-works-id="HitBlow"><a href="https://github.com/onion2424/Hit-and-Blow" target="_blank" rel="noopener noreferrer" class="svelte-5mclll"></a> 
+      <input type="checkbox" class="svelte-5mclll"/> 
+      <div class="headline svelte-5mclll"><h2>Hit&amp;Blow</h2> 
+        <h3>\u6700\u5584\u624B\u3068\u601D\u308F\u308C\u308B\u30B3\u30FC\u30EB\u3092\u6559\u3048\u3066\u304F\u308C\u308B\u30B3\u30F3\u30BD\u30FC\u30EB\u30A2\u30D7\u30EA</h3> 
+        <p class="svelte-5mclll">\u53CB\u3060\u3061\u306B\u52DD\u3064\u305F\u3081\u306B\u4F5C\u3063\u305F\u304C3\u6841\u3067\u306F\u4E94\u5206\u4E94\u5206\u3060\u3063\u305F</p></div> 
+      <div style="background-image: url(&#39;../static/image/hitblow.png&#39;)" alt="this_site" class="image svelte-5mclll"></div> 
+      <div class="message svelte-5mclll">\u30E1\u30C3\u30BB\u30FC\u30B8\u3067\u3059</div></div> 
+
+    <div class="work black svelte-5mclll" data-works-id="PuyoPuyo"><a href="https://github.com/onion2424/puyopuyo" target="_blank" rel="noopener noreferrer" class="svelte-5mclll"></a> 
+      <input type="checkbox" class="svelte-5mclll"/> 
+      <div class="headline svelte-5mclll"><h2>PuyoPuyo</h2> 
+        <h3>\u52DD\u624B\u306B\u9023\u9396\u3092\u7D44\u3093\u3067\u304F\u308C\u308BWin\u30A2\u30D7\u30EA</h3> 
+        <p class="svelte-5mclll">\u3077\u3088\u3077\u3088AI\u306E\u4ED5\u7D44\u307F\u304C\u6C17\u306B\u306A\u3063\u305F\u306E\u3067\u4F5C\u6210</p></div> 
+      <div class="video svelte-5mclll"><div class="video_container svelte-5mclll"><div class="iframe_wrapper svelte-5mclll"><video loop="" muted="" autoplay="" playsinline="" class="svelte-5mclll"><source src="/static/image/puyo.mp4"/></video></div></div></div> 
+      <div class="message svelte-5mclll">\u30E1\u30C3\u30BB\u30FC\u30B8\u3067\u3059</div></div></div>`,n(e,"class","svelte-5mclll")},m(t,i){p(t,e,i)},p:a,i:a,o:a,d(t){t&&h(e)}}}class k extends d{constructor(e){super();r(this,e,null,g,m,{})}}new k({target:document.getElementById("app")});
